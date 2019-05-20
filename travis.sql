87b0fde9668c5a97e63846be486e0a014b2b9412
Cameron Marsh <cmarsh@pillartechnology.com>
2019-05-20 12:02:23 -0400

    add database entries to travis build

---
+++ travis.sql
@@ -0,0 +1,29 @@
+# create test user
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
+
+# Create DB
+CREATE DATABASE IF NOT EXISTS `books` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `books`;
+
+# Create Table
+CREATE TABLE IF NOT EXISTS `test` (
+`title` varchar(50) NOT NULL,
+`author` varchar(50),
+`published` date,
+`read` boolean,
+)
+
+ALTER TABLE `test` ADD PRIMARY KEY (`title`);
+
+# Insert test cases into table if they do not exist
+REPLACE INTO `test`
+    (title, author, published, done)
+    VALUES
+    ('In Search of Lost Time', 'Marcel Proust', '1890-06-24', 1),
+    ('Walden', 'Henry David Thoreau', '1899-12-04', 1),
+    ('My Struggle', 'Karl Ove Knausgaard', '2009-10-20', 0),
+    ('The Count of Monte Cristo', 'Alexandre Dumas', '1906-03-28', 1),
+    ('The Sound and the Fury', 'William Faulkner', '1968-04-24', 0);
+
+
